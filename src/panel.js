0a9cee33379507b92a07805848d61407cab804ee
Tom Cooke <TomCookeDeveloper@gmail.com>
2022-06-22 20:03:47 +0100

    Comment about why we need extra event listener

--- src/panel.js
+++ src/panel.js
@@ -27,6 +27,7 @@ function createPanel(id, buttons) {
                 if (event === 'click') {
                     // If we're handling click we don't want to inherit the touchstart from body
                     button.addEventListener('touchstart', x => { x.stopPropagation(); }, { "passive": true });
+                    // Need this for iOS9 support
                     button.addEventListener('touchend', Utils.touchWrapper(buttons[i].eventListeners[event]));
                 }
             }
